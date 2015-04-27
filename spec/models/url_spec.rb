f1d173ac23086e8d673af5765148949766b7415d
Beal <jsbeal@gmail.com>
2015-04-26 23:29:47 -0600

    Whitespace

--- spec/models/url_spec.rb
+++ spec/models/url_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe Url, type: :model do
   describe "#primary_key" do
     it "should use short_url as the primary key for find methods" do
       u = Url.create :short_url => "http://babylin.ks/short", :long_url => "http://example.com?thisisreallytoolong"
-
       u2 = Url.find("http://babylin.ks/short")
 
       expect(u2).to eq(u)
